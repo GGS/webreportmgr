a02e1a2cd0f4486802907d2b43fa7beee1b3c0ea
GGS <ggs@gazinfo.com>
2016-07-27 10:03:32 +0500

    Common function Linux and FreeBSD

--- src/poll.erl
+++ src/poll.erl
@@ -43,20 +43,14 @@ exist(Initcount, Ostype) ->
     end,
     gproc:send({p, l, {pubsub,wsbroadcast}}, {self(), {pubsub,wsbroadcast}, term_to_binary({ostype,Ostype})}), 
     if Ostype == "Linux\n" ->
-            [L1,L5,L10,_,_] = string:tokens(os:cmd("cat /proc/loadavg")," "),
-            R = lists:filter(fun(X) -> X =/= nomatch  end, 
-                             lists:map(fun(X) -> re:run(X, "cpu\\d.+",[{capture,all,list}]) 
-                                       end, string:tokens(os:cmd("cat /proc/stat"),"\n"))),
-            T = lists:map(fun(X) -> {match, [Cpu]} = X, 
-                                    [Pu, User, Nice,Sys,Idle,Io,Irq,Sirq,Steal,Guest,Var1] = string:tokens(Cpu," "), 
-                                    Total = list_to_integer(User)+list_to_integer(Nice)+list_to_integer(Sys)+list_to_integer(Idle)+list_to_integer(Io)+list_to_integer(Irq)+list_to_integer(Sirq)+list_to_integer(Steal)+list_to_integer(Guest)+list_to_integer(Var1),
-                                    [Pu, Total, list_to_integer(Idle)] end, R),
-            Stat =  term_to_binary({procs,T}),
-            Lavg = term_to_binary({loadavg,[L1,L5,L10]}),
-            gproc:send({p, l, {pubsub,wsbroadcast}}, {self(), {pubsub,wsbroadcast}, Stat}), 
-            gproc:send({p, l, {pubsub,wsbroadcast}}, {self(), {pubsub,wsbroadcast}, Lavg});
+            Avgpath = "cat /proc/loadavg",
+            Cpupath = "cat /proc/stat",
+            ospath(Avgpath, Cpupath);
+               
        true  ->
-             gproc:send({p, l, {pubsub,wsbroadcast}}, {self(), {pubsub,wsbroadcast}, Ostype}) 
+            Avgpath = "cat /compat/linux/proc/loadavg",
+            Cpupath = "cat /compat/linux/proc/stat",
+            ospath(Avgpath, Cpupath)
     end,
     {M0,{M1,M2,M3}} = calendar:seconds_to_daystime(calendar:datetime_to_gregorian_seconds(calendar:now_to_local_time(now()))-Initcount),
     Uptime = term_to_binary({uptime,[integer_to_list(M0),M1,M2,M3]}),
@@ -69,3 +63,17 @@ exist(Initcount, Ostype) ->
     {noreply, {Timer,Initcount, Ostype}}.  
 
 
+ospath(Avgpath, Cpupath) ->
+    [L1,L5,L10,_,_] = string:tokens(os:cmd(Avgpath)," "),
+    R = lists:filter(fun(X) -> X =/= nomatch  end, 
+                     lists:map(fun(X) -> re:run(X, "cpu\\d.+",[{capture,all,list}]) 
+                               end, string:tokens(os:cmd(Cpupath),"\n"))),
+    T = lists:map(fun(X) -> {match, [Cpu]} = X, 
+                            [Pu, User, Nice,Sys,Idle,Io,Irq,Sirq,Steal,Guest,Var1] = string:tokens(Cpu," "), 
+                            Total = list_to_integer(User)+list_to_integer(Nice)+list_to_integer(Sys)+list_to_integer(Idle)+list_to_integer(Io)+list_to_integer(Irq)+list_to_integer(Sirq)+list_to_integer(Steal)+list_to_integer(Guest)+list_to_integer(Var1),
+                            [Pu, Total, list_to_integer(Idle)] end, R),
+    Stat =  term_to_binary({procs,T}),
+    Lavg = term_to_binary({loadavg,[L1,L5,L10]}),
+    gproc:send({p, l, {pubsub,wsbroadcast}}, {self(), {pubsub,wsbroadcast}, Stat}), 
+    gproc:send({p, l, {pubsub,wsbroadcast}}, {self(), {pubsub,wsbroadcast}, Lavg}),
+    {noreply}.
